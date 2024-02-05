a85f0c8a4e381dc291db91e08a48bc99f3055601
ximu3 <ximu_simo@outlook.com>
2024-02-05 22:25:08 +0800

    Update linkTitles.js
    fix change line problem

--- scripts/linkTitles.js
+++ scripts/linkTitles.js
@@ -12,7 +12,9 @@ hexo.extend.filter.register('before_post_render', function(data) {
     if (data.title !== title){
       const url = titleToUrlMap[title];
       const escapedTitle = escapeRegExp(title); // 转义标题
-      const regex = new RegExp(`${escapedTitle}($|(?=,)|\n)`, 'g');
+      // 修改正则表达式，添加捕获组以捕获 \n
+      const regex = new RegExp(`(${escapedTitle})($|(?=,))`, 'gm');
+      // 在替换字符串中使用 $1 来引用被捕获的标题，$2 引用尾随的字符（包括 \n）
       data.content = data.content.replace(regex, `[${title}](${url})`);
     }
   });
